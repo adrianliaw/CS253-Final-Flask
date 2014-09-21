4682dec94d31124df7a752a198a7bb40e7a0f13d
adrianliaw <adrianliaw2000@gmail.com>
2014-09-21 22:05:35 +0800

    mongo

--- appsetup.py
+++ appsetup.py
@@ -1,8 +1,9 @@
 from imports import *
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = os.getenv("MONGOSOUP_URL")
-app.config["MONGO_DBNAME"] = "cc_jtMPaDVgCNde"
+# app.config["MONGO_URI"] = os.getenv("MONGOSOUP_URL")
+# app.config["MONGO_DBNAME"] = "cc_jtMPaDVgCNde"
+app.config["MONGO_DBNAME"] = "cs253-final"
 app.secret_key = '\xfa\xde\x02\xa6^\xd9\xfb\xcd\x0e\x7f\xfc\xe9\xa4\x15\x9e\x14\x90i\x95\xa6\xfd-\x96\xb3'
 mongo = PyMongo(app)
 with app.app_context():
